623b88a3d22400a35ed42ebe0414a76a9aa88ece
thomashavermans <thomashavermans@mbp-van-thomas.home>
2017-07-11 16:26:34 +0200

    afwerken van templates voor modals waarvan data uit array wordt gehaald

--- docu.js
+++ docu.js
@@ -1,130 +1,201 @@
 let castarray = [
   {
-    'name': 'CHALICE <br> SOUNDSYSTEM',
+    'name': 'CHALICE SOUNDSYSTEM',
     'class': 'chalice',
+    'info': 'Lorem Ipsum',
     'imgtag': 'CHALICE%20SOUNDSYSTEM%202.png',
+    'website': 'www.google.com',
+    'abouttext': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla imperdiet volutpat. Aenean lobortis finibus risus et ullamcorper. Nam ultricies massa porttitor risus porttitor, sit amet vulputate purus vestibulum. Praesent dictum est et nibh varius, vel consectetur lectus efficitur. Nulla feugiat, est at aliquam feugiat, odio tortor consectetur lorem, eget pharetra lacus quam quis dolor.',
   },
   {
     'name': 'CHANNEL ONE SOUNDSYSTEM',
     'class': 'channelOne',
+    'info': 'Lorem Ipsum',
     'imgtag': 'CHANNEL%20ONE%20SOUNDSYSTEM.png',
+    'website': '',
+    'abouttext': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla imperdiet volutpat. Aenean lobortis finibus risus et ullamcorper. Nam ultricies massa porttitor risus porttitor, sit amet vulputate purus vestibulum. Praesent dictum est et nibh varius, vel consectetur lectus efficitur. Nulla feugiat, est at aliquam feugiat, odio tortor consectetur lorem, eget pharetra lacus quam quis dolor.',
   },
   {
     'name': 'CONGO ASHANTI ROY',
     'class': 'congo',
+    'info': 'Lorem Ipsum',
     'imgtag': 'CONGO%20ASHANTI%20ROY.png',
+    'website': '',
+    'abouttext': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla imperdiet volutpat. Aenean lobortis finibus risus et ullamcorper. Nam ultricies massa porttitor risus porttitor, sit amet vulputate purus vestibulum. Praesent dictum est et nibh varius, vel consectetur lectus efficitur. Nulla feugiat, est at aliquam feugiat, odio tortor consectetur lorem, eget pharetra lacus quam quis dolor.',
   },
   {
     'name': 'CRUCIAL ALPHONSO',
     'class': 'crucialA',
+    'info': 'Lorem Ipsum',
     'imgtag': 'CRUCIAL%20ALPHONSO.png',
+    'website': '',
+    'abouttext': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla imperdiet volutpat. Aenean lobortis finibus risus et ullamcorper. Nam ultricies massa porttitor risus porttitor, sit amet vulputate purus vestibulum. Praesent dictum est et nibh varius, vel consectetur lectus efficitur. Nulla feugiat, est at aliquam feugiat, odio tortor consectetur lorem, eget pharetra lacus quam quis dolor.',
   },
   {
     'name': 'CRUCIAL P',
     'class': 'crucialP',
+    'info': 'Lorem Ipsum',
     'imgtag': 'CRUCIAL%20P.png',
+    'website': '',
+    'abouttext': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla imperdiet volutpat. Aenean lobortis finibus risus et ullamcorper. Nam ultricies massa porttitor risus porttitor, sit amet vulputate purus vestibulum. Praesent dictum est et nibh varius, vel consectetur lectus efficitur. Nulla feugiat, est at aliquam feugiat, odio tortor consectetur lorem, eget pharetra lacus quam quis dolor.',
   },
   {
     'name': 'DOCTOR DOG',
     'class': 'drDog',
+    'info': 'Lorem Ipsum',
     'imgtag': 'DOCTOR%20DOG.png',
+    'website': '',
+    'abouttext': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla imperdiet volutpat. Aenean lobortis finibus risus et ullamcorper. Nam ultricies massa porttitor risus porttitor, sit amet vulputate purus vestibulum. Praesent dictum est et nibh varius, vel consectetur lectus efficitur. Nulla feugiat, est at aliquam feugiat, odio tortor consectetur lorem, eget pharetra lacus quam quis dolor.',
   },
   {
     'name': 'DON WACKIE',
     'class': 'donW',
+    'info': 'Lorem Ipsum',
     'imgtag': 'DON%20WACKIE.png',
+    'website': '',
+    'abouttext': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla imperdiet volutpat. Aenean lobortis finibus risus et ullamcorper. Nam ultricies massa porttitor risus porttitor, sit amet vulputate purus vestibulum. Praesent dictum est et nibh varius, vel consectetur lectus efficitur. Nulla feugiat, est at aliquam feugiat, odio tortor consectetur lorem, eget pharetra lacus quam quis dolor.',
   },
   {
     'name': 'FATMAN',
     'class': 'fatman',
+    'info': 'Lorem Ipsum',
     'imgtag': 'FATMAN.png',
+    'website': '',
+    'abouttext': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla imperdiet volutpat. Aenean lobortis finibus risus et ullamcorper. Nam ultricies massa porttitor risus porttitor, sit amet vulputate purus vestibulum. Praesent dictum est et nibh varius, vel consectetur lectus efficitur. Nulla feugiat, est at aliquam feugiat, odio tortor consectetur lorem, eget pharetra lacus quam quis dolor.',
   },
   {
     'name': 'JAH VOICE SOUNDSYSTEM',
     'class': 'jahV',
+    'info': 'Lorem Ipsum',
     'imgtag': 'JAH%20VOICE%20SOUNDSYSTEM.png',
+    'website': '',
+    'abouttext': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla imperdiet volutpat. Aenean lobortis finibus risus et ullamcorper. Nam ultricies massa porttitor risus porttitor, sit amet vulputate purus vestibulum. Praesent dictum est et nibh varius, vel consectetur lectus efficitur. Nulla feugiat, est at aliquam feugiat, odio tortor consectetur lorem, eget pharetra lacus quam quis dolor.',
   },
   {
     'name': 'JAHMBASSADOR',
     'class': 'jahmB',
+    'info': 'Lorem Ipsum',
     'imgtag': 'JAHMBASSADOR.png',
+    'website': '',
+    'abouttext': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla imperdiet volutpat. Aenean lobortis finibus risus et ullamcorper. Nam ultricies massa porttitor risus porttitor, sit amet vulputate purus vestibulum. Praesent dictum est et nibh varius, vel consectetur lectus efficitur. Nulla feugiat, est at aliquam feugiat, odio tortor consectetur lorem, eget pharetra lacus quam quis dolor.',
   },
   {
     'name': 'KAREL MICHIELS',
     'class': 'KarelM',
+    'info': 'Lorem Ipsum',
     'imgtag': 'KAREL%20MICHIELS.png',
+    'website': '',
+    'abouttext': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla imperdiet volutpat. Aenean lobortis finibus risus et ullamcorper. Nam ultricies massa porttitor risus porttitor, sit amet vulputate purus vestibulum. Praesent dictum est et nibh varius, vel consectetur lectus efficitur. Nulla feugiat, est at aliquam feugiat, odio tortor consectetur lorem, eget pharetra lacus quam quis dolor.',
   },
   {
     'name': 'KIANGANA',
     'class': 'kiangana',
+    'info': 'Lorem Ipsum',
     'imgtag': 'KIANGANA.png',
+    'website': '',
+    'abouttext': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla imperdiet volutpat. Aenean lobortis finibus risus et ullamcorper. Nam ultricies massa porttitor risus porttitor, sit amet vulputate purus vestibulum. Praesent dictum est et nibh varius, vel consectetur lectus efficitur. Nulla feugiat, est at aliquam feugiat, odio tortor consectetur lorem, eget pharetra lacus quam quis dolor.',
   },
   {
     'name': 'KING FLASHMAN',
     'class': 'kingF',
+    'info': 'Lorem Ipsum',
     'imgtag': 'KING%20FLASHMAN.png',
+    'website': '',
+    'abouttext': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla imperdiet volutpat. Aenean lobortis finibus risus et ullamcorper. Nam ultricies massa porttitor risus porttitor, sit amet vulputate purus vestibulum. Praesent dictum est et nibh varius, vel consectetur lectus efficitur. Nulla feugiat, est at aliquam feugiat, odio tortor consectetur lorem, eget pharetra lacus quam quis dolor.',
   },
   {
     'name': 'LLOYD COXSONE',
     'class': 'coxsone',
+    'info': 'Lorem Ipsum',
     'imgtag': 'LLOYD%20COXSONE.png',
+    'website': '',
+    'abouttext': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla imperdiet volutpat. Aenean lobortis finibus risus et ullamcorper. Nam ultricies massa porttitor risus porttitor, sit amet vulputate purus vestibulum. Praesent dictum est et nibh varius, vel consectetur lectus efficitur. Nulla feugiat, est at aliquam feugiat, odio tortor consectetur lorem, eget pharetra lacus quam quis dolor.',
   },
   {
     'name': 'LYRICAL BENJIE',
     'class': 'benjie',
+    'info': 'Lorem Ipsum',
     'imgtag': 'LYRICAL%20BENJIE.png',
+    'website': '',
+    'abouttext': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla imperdiet volutpat. Aenean lobortis finibus risus et ullamcorper. Nam ultricies massa porttitor risus porttitor, sit amet vulputate purus vestibulum. Praesent dictum est et nibh varius, vel consectetur lectus efficitur. Nulla feugiat, est at aliquam feugiat, odio tortor consectetur lorem, eget pharetra lacus quam quis dolor.',
   },
   {
     'name': 'MISSING LINK',
     'class': 'missingL',
+    'info': 'Lorem Ipsum',
     'imgtag': 'MISSING%20LINK.png',
+    'website': '',
+    'abouttext': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla imperdiet volutpat. Aenean lobortis finibus risus et ullamcorper. Nam ultricies massa porttitor risus porttitor, sit amet vulputate purus vestibulum. Praesent dictum est et nibh varius, vel consectetur lectus efficitur. Nulla feugiat, est at aliquam feugiat, odio tortor consectetur lorem, eget pharetra lacus quam quis dolor.',
   },
   {
     'name': 'NOMADIX',
     'class': 'nomadix',
+    'info': 'Lorem Ipsum',
     'imgtag': 'NOMADIX.png',
+    'website': '',
+    'abouttext': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla imperdiet volutpat. Aenean lobortis finibus risus et ullamcorper. Nam ultricies massa porttitor risus porttitor, sit amet vulputate purus vestibulum. Praesent dictum est et nibh varius, vel consectetur lectus efficitur. Nulla feugiat, est at aliquam feugiat, odio tortor consectetur lorem, eget pharetra lacus quam quis dolor.',
   },
   {
     'name': 'PANACHE CULTURE',
     'class': 'panache',
+    'info': 'Lorem Ipsum',
     'imgtag': 'PANACHE%20CULTURE.png',
+    'website': '',
+    'abouttext': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla imperdiet volutpat. Aenean lobortis finibus risus et ullamcorper. Nam ultricies massa porttitor risus porttitor, sit amet vulputate purus vestibulum. Praesent dictum est et nibh varius, vel consectetur lectus efficitur. Nulla feugiat, est at aliquam feugiat, odio tortor consectetur lorem, eget pharetra lacus quam quis dolor.',
   },
   {
     'name': 'PROFESSOR CAT',
     'class': 'profCat',
+    'info': 'Lorem Ipsum',
     'imgtag': 'PROFESSOR%20CAT.png',
+    'website': '',
+    'abouttext': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla imperdiet volutpat. Aenean lobortis finibus risus et ullamcorper. Nam ultricies massa porttitor risus porttitor, sit amet vulputate purus vestibulum. Praesent dictum est et nibh varius, vel consectetur lectus efficitur. Nulla feugiat, est at aliquam feugiat, odio tortor consectetur lorem, eget pharetra lacus quam quis dolor.',
   },
   {
     'name': 'PURAMAN',
     'class': 'puraman',
+    'info': 'Lorem Ipsum',
     'imgtag': 'PURAMAN.png',
+    'website': '',
+    'abouttext': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla imperdiet volutpat. Aenean lobortis finibus risus et ullamcorper. Nam ultricies massa porttitor risus porttitor, sit amet vulputate purus vestibulum. Praesent dictum est et nibh varius, vel consectetur lectus efficitur. Nulla feugiat, est at aliquam feugiat, odio tortor consectetur lorem, eget pharetra lacus quam quis dolor.',
   },
   {
     'name': 'RADICS',
     'class': 'radics',
+    'info': 'Lorem Ipsum',
     'imgtag': 'RADICS.png',
+    'website': '',
+    'abouttext': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla imperdiet volutpat. Aenean lobortis finibus risus et ullamcorper. Nam ultricies massa porttitor risus porttitor, sit amet vulputate purus vestibulum. Praesent dictum est et nibh varius, vel consectetur lectus efficitur. Nulla feugiat, est at aliquam feugiat, odio tortor consectetur lorem, eget pharetra lacus quam quis dolor.',
   },
   {
     'name': 'RAS MUFFET',
     'class': 'rasM',
+    'info': 'Lorem Ipsum',
     'imgtag': 'RAS%20MUFFET.png',
+    'website': '',
+    'abouttext': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla imperdiet volutpat. Aenean lobortis finibus risus et ullamcorper. Nam ultricies massa porttitor risus porttitor, sit amet vulputate purus vestibulum. Praesent dictum est et nibh varius, vel consectetur lectus efficitur. Nulla feugiat, est at aliquam feugiat, odio tortor consectetur lorem, eget pharetra lacus quam quis dolor.',
   },
   {
     'name': 'RIGO VERWIMP',
     'class': 'rigoV',
+    'info': 'Lorem Ipsum',
     'imgtag': 'RIGO%20VERWIMP.png',
+    'website': '',
+    'abouttext': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla imperdiet volutpat. Aenean lobortis finibus risus et ullamcorper. Nam ultricies massa porttitor risus porttitor, sit amet vulputate purus vestibulum. Praesent dictum est et nibh varius, vel consectetur lectus efficitur. Nulla feugiat, est at aliquam feugiat, odio tortor consectetur lorem, eget pharetra lacus quam quis dolor.',
   },
   {
     'name': 'RUSS D',
     'class': 'russD',
+    'info': 'Lorem Ipsum',
     'imgtag': 'RUSS%20D.png',
+    'website': '',
+    'abouttext': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla imperdiet volutpat. Aenean lobortis finibus risus et ullamcorper. Nam ultricies massa porttitor risus porttitor, sit amet vulputate purus vestibulum. Praesent dictum est et nibh varius, vel consectetur lectus efficitur. Nulla feugiat, est at aliquam feugiat, odio tortor consectetur lorem, eget pharetra lacus quam quis dolor.',
   }
 ]
 
 let cast_div = document.getElementById('castGrid');
 let cast_text = document.getElementsByClassName('hoverText');
 let modaldoc = document.getElementsByClassName('modaldocu')[0];
-let tellerdoc = 0;
 
 for (var i = 0; i < castarray.length; i++) {
   let cast = document.createElement('div');
@@ -143,31 +214,30 @@ for (var i = 0; i < castarray.length; i++) {
   cast_link.innerHTML = castarray[i].name;
   let hover_text = document.getElementsByClassName('hoverText');
   for (var i = 0; i < hover_text.length; i++) {
-    hover_text[i].setAttribute('imgalt', 'Assets/img/CAST/' + castarray[i].imgtag, )
+    hover_text[i].setAttribute('imgalt', 'Assets/img/CAST/' + castarray[i].imgtag)
+    hover_text[i].setAttribute('artistinfo', castarray[i].info)
+    hover_text[i].setAttribute('website', castarray[i].website)
+    hover_text[i].setAttribute('aboutinfo', castarray[i].abouttext)
   }
   overlay.appendChild(cast_link);
+}
 
-
-  // for (var i = 0; i < cast_text.length; i++) {
-  //   cast_text[i].onclick = function() {
-  //     if (tellerdoc == 1) {
-  //       tellerdoc = 0;
-  //     }
-  //
-  //     else {
-  //       console.log(this.getAttribute('data-test'));
-  //       modaldoc.style.opacity = 1;
-  //       modaldoc.style.visibility = 'visible';
-  //       document.getElementsByClassName('docu-foto')[0].src = this.getAttribute('imgalt');
-  //       tellerdoc++
-  //     }
-  //   }
-  // }
+for (var i = 0; i < cast_text.length; i++) {
+  cast_text[i].onclick = function() {
+      console.log(this.getAttribute('data-test'));
+      modaldoc.style.opacity = 1;
+      modaldoc.style.visibility = 'visible';
+      document.getElementsByClassName('docu-foto')[0].src = this.getAttribute('imgalt');
+      document.getElementById('name').innerHTML = this.id;
+      document.getElementById('filter').innerHTML = this.getAttribute('artistinfo');
+      document.getElementById('website').href = "http://" + this.getAttribute('website');
+      document.getElementById('about').innerHTML = "Over " + this.id;
+      document.getElementById('info').innerHTML = this.getAttribute('aboutinfo');
+  }
 }
 
 window.onclick = function(event) {
     if (event.target == modaldoc) {
-      tellerdoc = 0;
       modaldoc.style.opacity = 0;
       modaldoc.style.visibility = 'hidden'
     }
